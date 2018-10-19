21b5478128f1a058b03d742683a83852d0d297ab
Aliesia Pavliuk <alesia.lexy@gmail.com>
2018-10-19 14:05:34 +0200

    implemented Webdriver for chrome

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-const site = 'https://www.franmerpools.ru/';
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const OPTIONS = {
+    site: 'https://www.franmerpools.ru/',
+    screen:{
+        width: 1280,
+        height: 720
+    }
+};
 
-var edge = require('selenium-webdriver/edge');
-var service = new edge.ServiceBuilder()
-    .setPort(55555)
-    .build();
-var options = new edge.Options();
-// configure browser options ...
-var driver = edge.Driver.createSession(options, service);
+let chromeCapabilities = webdriver.Capabilities.chrome();
+let chromeOptions = new chrome.Options().windowSize(OPTIONS.screen);
+let driver = new webdriver.Builder().setChromeOptions(chromeOptions).withCapabilities(chromeCapabilities).build();
 
 
-driver.get(site)
-.then(_=> console.log('entered site'));
+driver.get(OPTIONS.site)
+    .then(_=> console.log('entered site'))
+    .then (_=> driver.quit(driver));
