78e5167ab04e4668cd715a63df3d7cc3c612037e
Alice Pavliuk <alesia.lexy@gmail.com>
2018-10-25 02:11:25 +0200

    Checked templates with models

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const By = webdriver.By;
 const until = webdriver.until;
 const chrome = require('selenium-webdriver/chrome');
 const OPTIONS = {
-    site: 'http://www.franmerpools.ru/',
+    site: 'https://www.franmerpools.ru/',
     screen:{
         width: 1280,
         height: 720
@@ -131,7 +131,7 @@ describe('Main page functionality',function() {
             .then(title => assert.equal(title, secondSlideTitle, 'Button on first template is not providing to the next template'))
     });
     // TODO:: How the svg-button is working?
-
+    // TODO:: should I test if map is working properly?
     it('Can see map on second template', function(){
         return driver.get(OPTIONS.site)
             .then(() => driver.wait(until.elementLocated(By.css('h2'))))
@@ -193,5 +193,103 @@ describe('Main page functionality',function() {
             .then(()=> driver.findElement(By.css('#secCall .left fieldset label')).getAttribute('input'))
             .catch(()=> assert.ok(false, 'form is not found'))
     });
+
+});
+
+
+describe('Main navigation templates',function(){
+    let driver;
+    this.timeout(30000);
+    beforeEach(function () {
+        let chromeCapabilities = webdriver.Capabilities.chrome();
+        let chromeOptions = new chrome.Options().windowSize(OPTIONS.screen);
+        driver = new webdriver.Builder().setChromeOptions(chromeOptions).withCapabilities(chromeCapabilities).build();
+    });
+    afterEach(function () {
+        driver.close();
+    });
+    function getElementByCss(path) {
+        return driver.wait(until.elementLocated(By.css(path)))
+            .then(()=> driver.sleep(1500))
+            .then(()=> driver.findElement(By.css(path)));
+    }
+    it('Can see filter of models', function () {
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.filter-list li:nth-child(1)'))))
+            .then(()=> driver.findElement(By.css('.filter-list')))
+            .catch(()=> assert.ok(false, 'Can not find element on page'))
+    });
+    it('Can see models on model series', function () {
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.list-models .l-m-item:nth-child(2)'))))
+            .then(()=> driver.findElement(By.css('.list-models')))
+            .catch(()=> assert.ok(false, 'Can not find element on page'))
+    });
+
+    it('Can enter details', function () {
+        let initialTitle;
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.list-models .l-m-item:nth-of-type(2) h4'))))
+            .then(()=> driver.sleep(2000))
+            .then(()=> driver.findElement(By.css('.list-models .l-m-item:nth-of-type(2) h4 a')).getAttribute('title'))
+            .then(tempTitle => {
+                    initialTitle = tempTitle;
+            })
+            .then(()=> driver.findElement(By.css('.list-models .l-m-item:nth-of-type(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('#ppb h1'))))
+            .then(()=> driver.sleep(2000))
+            .then(()=> driver.findElement(By.css('h1')).getText())
+            .then(modelTitle=> assert.equal(modelTitle, initialTitle, 'Can not find title on page'))
+    });
+
+    it('Can swipe pictures', function () {
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.list-models .l-m-item:nth-of-type(2) h4'))))
+            .then(()=> driver.sleep(1000))
+            .then(()=> driver.findElement(By.css('.list-models .l-m-item:nth-of-type(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('picture'))))
+            .then(()=> driver.sleep(1000))
+            .then(()=> driver.findElement(By.css('.arrow')).click())
+            .catch(()=> assert.ok(false, 'Can not find slider on page'))
+    });
+    it('Can see available colors of selected model', function () {
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.list-models .l-m-item:nth-of-type(2) h4'))))
+            .then(()=> driver.sleep(1000))
+            .then(()=> driver.findElement(By.css('.list-models .l-m-item:nth-of-type(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('#ppb #colors div figure picture img'))))
+            .then(()=> driver.findElement(By.css('#ppb #colors div figure picture img')).click())
+            .then(()=> driver.sleep(2000))
+            .then(()=> driver.findElement(By.css('.psh-closer')).click())
+            .catch(()=> assert.ok(false, 'Can not find available colors on page'))
+    });
+    it('Can see thesis', function () {
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.list-models .l-m-item:nth-of-type(2) h4'))))
+            .then(()=> driver.sleep(1500))
+            .then(()=> driver.findElement(By.css('.list-models .l-m-item:nth-of-type(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('#ppb #thesis div ul li h6'))))
+            .then(()=> driver.findElement(By.css('#ppb #thesis div ul li')).click())
+            .catch(()=> assert.ok(false, 'Can not find thesis on page'))
+    });
+    it('Can ask project cost', function () {
+        return driver.get(OPTIONS.site)
+            .then(async() => (await getElementByCss('.main-nav li:nth-child(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('.list-models .l-m-item:nth-of-type(2) h4'))))
+            .then(()=> driver.sleep(1000))
+            .then(()=> driver.findElement(By.css('.list-models .l-m-item:nth-of-type(2) a')).click())
+            .then(()=> driver.wait(until.elementLocated(By.css('#action2 button'))))
+            .catch(()=> assert.ok(false, 'Can not find button to ask project cost on page'))
+    });
+//TODO:: Button to ask for project cost is not standardised
+
+
+
 
 });
